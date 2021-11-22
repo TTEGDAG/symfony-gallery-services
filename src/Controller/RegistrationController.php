b6b8794906e821d1f0d13121e54e07ad60bafcf3
Adam Kostecki <kostecki.adam@gmail.com>
2021-11-22 20:33:45 +0100

    navbar login logout

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -20,6 +20,9 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginFormAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
+        if($this->getUser()){
+            return $this->redirectToRoute('index');
+        }
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
