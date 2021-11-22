fb745d129ca0ecff6a231ccf567b8a8c2c77e6ec
Adam Kostecki <kostecki.adam@gmail.com>
2021-11-22 22:31:48 +0100

    UploadedFile

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Photo;
 use App\Form\UploadPhotoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,9 +14,30 @@ class IndexController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $form = $this->createForm(UploadPhotoType::class);
+
+        //obsługa wysłanego formularza
+        $form->handleRequest($request);
+
+        //sprawdzamy czy formularz jest potwierdzony i poprawnie zwalidowany
+        if($form->isSubmitted() && $form->isValid()){
+
+            $em = $this->getDoctrine()->getManager();
+
+            //sprawdzamy czy użytkownik jest zalogowany
+            if($this->getUser()){
+                $entityPhotos = new Photo();
+                $entityPhotos->setFilename($form->get('filename')->getData());
+                $entityPhotos->setIsPublic($form->get('is_public')->getData());
+                $entityPhotos->setUploadedAt(new \DateTime());
+                $entityPhotos->setUser($this->getUser());
+
+                $em->persist($entityPhotos);
+                $em->flush();
+            }
+        }
         
         return $this->render('index/index.html.twig',[
             'form' => $form->createView(),
