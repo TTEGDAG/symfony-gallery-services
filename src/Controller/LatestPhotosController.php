53a29cba000ba535fad2060fdcaf06e67be4b31d
Adam Kostecki <kostecki.adam@gmail.com>
2021-11-30 19:11:37 +0100

    Latest Photos

--- src/Controller/LatestPhotosController.php
+++ src/Controller/LatestPhotosController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Photo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\component\Routing\Annotation\Route;
 
 class LatestPhotosController extends AbstractController
 {
     /**
-     * @Route("/latest", name="latest_photo")
+     * @Route("/latest", name="latest_photos")
      */
     public function index()
     {
         $em = $this->getDoctrine()->getManager();
         $latestPhotosPublic = $em->getRepository( Photo::class)-> findBy(['is_public' => true]);
-        return $this->render( ' ', [
-            'latestPhotoPublic' => $latestPhotosPublic
+        return $this->render( 'latest_photos/index.html.twig', [
+            'latestPhotosPublic' => $latestPhotosPublic
         ] );
     }
 }
